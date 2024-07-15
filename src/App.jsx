6f4cce5e9f218d01ae8150c9e96b950eeb4cf6fc
pughaz2003 <pughaz668@gmail.com>
2024-07-15 12:13:55 +0530

    changes commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Home from './pages/Home';
 import SigUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
-import SignIn from './pages/SignIn';
+
+
 
 
 export default function App() {
   return  <BrowserRouter>
 <Routes>
   <Route path='/' element={<Home/>}/>
-  <Route path='/sign-in' element={<SignIn/>}/>
   <Route path='/sign-up' element={<SigUp/>}/>
   <Route path='/about' element={<About/>}/>
   <Route path='/profile' element={<Profile/>}/>
