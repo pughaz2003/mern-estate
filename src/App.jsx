ade97b3087c4f5eab29e64bc456dc2ff88ac0a5b
pughaz2003 <pughaz668@gmail.com>
2024-07-15 15:19:16 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,15 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
-import TempSignIn from './pages/SignIn';
-
+import SignIn from './pages/SignIn';
 
 
 
 export default function App() {
   return  <BrowserRouter>
 <Routes>
-  <Route path='/' element={<Home/>}/>
-  <Route path='/sign-in' element={<signIn/>}/>
+  <Route path='/home' element={<Home/>}/>
+  <Route path='/sign-in' element={<SignIn/>}/>
   <Route path='/sign-up' element={<SignUp/>}/>
   <Route path='/about' element={<About/>}/>
   <Route path='/profile' element={<Profile/>}/>
