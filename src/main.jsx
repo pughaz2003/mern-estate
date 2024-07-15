d51a05cbe4408af3333639c09f840bfaa8f71470
pughaz2003 <pughaz668@gmail.com>
2024-07-15 11:14:01 +0530

    install reactjs  and tailwind css

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import Nav from './Nav'
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <App/>
+   <Nav/>
+   
+    
+  </React.StrictMode>,
+)
