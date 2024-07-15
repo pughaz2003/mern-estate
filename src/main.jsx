0bcca32997ce9acbde11c43a94abaea07a48169a
pughaz2003 <pughaz668@gmail.com>
2024-07-15 11:22:32 +0530

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import Nav from './Nav'
 
 
 
@@ -11,7 +10,7 @@ import Nav from './Nav'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <App/>
-   <Nav/>
+   
    
     
   </React.StrictMode>,
