52f8f1ee2e5df10fa530e716e636f878d1b3db8a
pughaz2003 <pughaz668@gmail.com>
2024-07-25 12:44:08 +0530

    changes committed

--- api/models/User.model.js
+++ api/models/User.model.js
@@ -16,8 +16,8 @@ const userSchema =new mongoose.Schema({
         required: true,
         
     }
-},{timestamps: true });
+    },{ timestamps: true });
 
-const User =mongoose.model('User, userSchema');
+const User = mongoose.model('User', userSchema);
 
 export default User;
